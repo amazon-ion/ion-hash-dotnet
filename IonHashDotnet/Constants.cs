18dde2d487243cfc998033e4076bf6beff197d75
Simon Zhao <simonz@bitquilltech.com>
2019-12-04 16:00:32 -0800

    Fix missing constants

---
+++ IonHashDotnet/Constants.cs
@@ -0,0 +1,13 @@
+﻿namespace IonHashDotnet
+{
+    internal static class Constants
+    {
+        internal static readonly byte BeginMarkerByte = 0x0B;
+        internal static readonly byte EndMarkerByte = 0x0E;
+        internal static readonly byte EscapeByte = 0x0C;
+        internal static readonly byte[] BeginMarker = { BeginMarkerByte };
+        internal static readonly byte[] EndMarker = { EndMarkerByte };
+
+        internal static readonly byte[] TqAnnotatedValue = { 0xE0 };
+    }
+}
