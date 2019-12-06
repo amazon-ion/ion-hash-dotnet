51e38c24433326a9aa0f4f2ca37de33949607113
Byron Lin <byronl@bitquilltech.com>
2019-12-06 15:10:20 -0800

    Address PR comments p2

--- IonHashDotnet/Constants.cs
+++ IonHashDotnet/Constants.cs
@@ -7,6 +7,7 @@
         internal static readonly byte EscapeByte = 0x0C;
         internal static readonly byte[] BeginMarker = { BeginMarkerByte };
         internal static readonly byte[] EndMarker = { EndMarkerByte };
+
         internal static readonly byte[] TqAnnotatedValue = { 0xE0 };
     }
 }
