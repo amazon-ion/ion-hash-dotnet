87745b33f222afeb76428ccb184823c5a4e611bd
Simon Zhao <42222408+simonz-bq@users.noreply.github.com>
2020-01-06 11:13:30 -0800

    Implement StructSerializer (#4)

--- IonHashDotnet/StructSerializer.cs
+++ IonHashDotnet/StructSerializer.cs
@@ -1,30 +1,63 @@
 ﻿namespace IonHashDotnet
 {
-    using System;
+    using System.Collections.Generic;
 
     internal class StructSerializer : Serializer
     {
+        private static readonly DigestComparer Comparer = new DigestComparer();
         private readonly Serializer scalarSerializer;
-        private readonly byte[] fieldHashes;
+        private readonly List<byte[]> fieldHashes;
 
         internal StructSerializer(IIonHasher hashFunction, int depth, IIonHasherProvider hashFunctionProvider)
             : base(hashFunction, depth)
         {
+            this.scalarSerializer = new Serializer(hashFunctionProvider.NewHasher(), depth + 1);
+            this.fieldHashes = new List<byte[]>();
         }
 
-        internal void Scalar(IIonValue value)
+        internal override void Scalar(IIonValue value)
         {
-            throw new NotImplementedException();
+            this.scalarSerializer.HandleFieldName(value.FieldName);
+            this.scalarSerializer.Scalar(value);
+            byte[] digest = this.scalarSerializer.Digest();
+            this.AppendFieldHash(digest);
         }
 
-        internal void StepOut()
+        internal override void StepOut()
         {
-            throw new NotImplementedException();
+            this.fieldHashes.Sort(Comparer);
+            foreach (byte[] digest in this.fieldHashes)
+            {
+                this.Update(Escape(digest));
+            }
+
+            base.StepOut();
         }
 
         internal void AppendFieldHash(byte[] digest)
         {
-            throw new NotImplementedException();
+            this.fieldHashes.Add(digest);
+        }
+
+        private class DigestComparer : IComparer<byte[]>
+        {
+            public int Compare(byte[] a, byte[] b)
+            {
+                var i = 0;
+                while (i < a.Length && i < b.Length)
+                {
+                    byte aByte = a[i];
+                    byte bByte = b[i];
+                    if (!aByte.Equals(bByte))
+                    {
+                        return aByte - bByte;
+                    }
+                    
+                    i++;
+                }
+
+                return a.Length - b.Length;
+            }
         }
     }
 }
