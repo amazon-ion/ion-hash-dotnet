6f0cb42af08f056de793c41a76e2934c721bc33a
Simon Zhao <simonz@bitquilltech.com>
2020-01-10 13:58:39 -0800

    HasherProvider WIP

--- IonHashDotnet.Tests/TestIonHasherProviders.cs
+++ IonHashDotnet.Tests/TestIonHasherProviders.cs
@@ -1,10 +1,105 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using IonDotnet;
+using IonDotnet.Tree;
+using IonDotnet.Tree.Impl;
 
 namespace IonHashDotnet.Tests
 {
-    class TestIonHasherProviders
+    internal class TestIonHasherProviders
     {
+        private static TestIonHasherProvider GetInstance(string algorithm)
+        {
+            if (algorithm.Equals("identity"))
+            {
+                return new IdentityIonHasherProvider();
+            }
+            else
+            {
+                return new DefaultTestIonHasherProvider(algorithm);
+            }
+        }
+
+        internal abstract class TestIonHasherProvider : IIonHasherProvider
+        {
+            private static readonly ValueFactory valueFactory = new ValueFactory();
+            private readonly IIonValue hashLog = valueFactory.NewEmptySexp();
+
+            public abstract IIonHasher NewHasher();
+
+            public void AddHashToLog(string method, byte[] hash)
+            {
+                var node = valueFactory.NewEmptySexp();
+                node.AddTypeAnnotation(new SymbolToken(method, SymbolToken.UnknownSid));
+                foreach (byte b in hash)
+                {
+                    node.Add(valueFactory.NewInt(b & 0xFF));
+                }
+
+                hashLog.Add(node);
+            }
+
+            public IIonValue GetHashLog()
+            {
+                return this.hashLog;
+            }
+        }
+
+        internal class IdentityIonHasherProvider : TestIonHasherProvider
+        {
+            public override IIonHasher NewHasher()
+            {
+                return new IdentityIonHasher();
+            }
+
+            private class IdentityIonHasher : IIonHasher
+            {
+                public byte[] Hash => throw new NotImplementedException();
+
+                public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
+
+        internal class DefaultTestIonHasherProvider : TestIonHasherProvider
+        {
+            private string algorithm;
+
+            public DefaultTestIonHasherProvider(string algorithm)
+            {
+                this.algorithm = algorithm;
+            }
+
+            public override IIonHasher NewHasher()
+            {
+                return new DefaultTestIonHasher(algorithm);
+            }
+
+            private class DefaultTestIonHasher : IIonHasher
+            {
+                public DefaultTestIonHasher(string algorithm)
+                {
+
+                }
+
+                public byte[] Hash => throw new NotImplementedException();
+
+                public int TransformBlock(byte[] inputBuffer, int inputOffset, int inputCount, byte[] outputBuffer, int outputOffset)
+                {
+                    throw new NotImplementedException();
+                }
+
+                public byte[] TransformFinalBlock(byte[] inputBuffer, int inputOffset, int inputCount)
+                {
+                    throw new NotImplementedException();
+                }
+            }
+        }
     }
 }
