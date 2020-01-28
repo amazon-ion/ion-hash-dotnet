75e8c1388e842232378906b8f9d8dcc060496023
Byron Lin <byronl@bitquilltech.com>
2020-01-27 16:29:01 -0800

    remove global suppressions file

--- IonHashDotnet.Tests/GlobalSuppressions.cs
+++
@@ -1,9 +0,0 @@
-﻿// This file is used by Code Analysis to maintain SuppressMessage
-// attributes that are applied to this project.
-// Project-level suppressions either have no target or are given
-// a specific target and scoped to a namespace, type, member, etc.
-
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.OrderingRules", "SA1200:Using directive should appear within a namespace declaration", Justification = "WIP")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.OrderingRules", "SA1633:The file header is missing or not located at the top of the file", Justification = "WIP")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.StyleCop.CSharp.OrderingRules", "SA0001:XML comment analysis is disabled due to project configuration", Justification = "WIP")]
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1600:Elements should be documented", Justification = "WIP")]
